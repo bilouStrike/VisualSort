5b17b0b1eb930bc560126cdf334ce7c4eec4c93b
Bilal <bilal89dif@gmail.com>
2023-06-19 21:24:57 +0200

    Updates

---
+++ precache-manifest.50b8fa9629c50c3e4ea302c42faf5dea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01524eb7db8e13facd29ca5df1c2fa71",
+    "url": "/VisualSort/index.html"
+  },
+  {
+    "revision": "fb27f3510a91286ba102",
+    "url": "/VisualSort/static/css/main.1c6580e0.chunk.css"
+  },
+  {
+    "revision": "8fa0f778d0ebc7636bfe",
+    "url": "/VisualSort/static/js/2.272a7300.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/VisualSort/static/js/2.272a7300.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb27f3510a91286ba102",
+    "url": "/VisualSort/static/js/main.487a3336.chunk.js"
+  },
+  {
+    "revision": "f29121dff3317a7ba681",
+    "url": "/VisualSort/static/js/runtime-main.8766dc12.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/VisualSort/precache-manifest.50b8fa9629c50c3e4ea302c42faf5dea.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/VisualSort/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
